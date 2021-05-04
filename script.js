99b64457a13bb2e4e00e66cd07de56f508ccc083
Devin Foster <ADGrace707@gmail.com>
2021-05-04 09:30:37 -0500

    Add functionality to display a book on page

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function Book(title, author, pages, read) {
 function addBook(book) {
     myBooks.push(book);
 }
+
+const bookList = document.getElementById('book-list');
+
+function displayBook(book) {
+    const bookItem = document.createElement('div');
+    bookItem.textContent = book.title;
+    bookList.appendChild(bookItem);
+}
+
+function displayAllBooks() {
+    bookList.innerHTML ="";
+    myBooks.map(item => displayBook(item));
+}
+
+const testBook0 = new Book('Test Title', 'Test Author', 100, false);
+const testBook1 = new Book('Test Title', 'Test Author', 100, false);
+
+addBook(testBook0);
+addBook(testBook1);
+
+
+displayAllBooks(myBooks);
